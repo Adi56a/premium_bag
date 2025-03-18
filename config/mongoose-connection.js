6a4b1b8f9c7780876481730a0380f41b7f97fab7
Aditya <adityajogdand15@gmail.com>
2025-03-18 23:44:48 +0530

    all configs

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,11 +1,12 @@
 const mongoose  = require('mongoose')
+const config = require('config')
 
-mongoose.connect('mongodb+srv://adityajogdand15:ugaZmh2bXP8URUoe@practice.5gmcg.mongodb.net/?retryWrites=true&w=majority&appName=practice')
+mongoose.connect(`${config.get("MONGODB_URI")}`)
 .then(function(){
-console.log('Connected to DataBase Successfully')
+ console.log('Connected to DataBase Successfully')
 })
 .catch(function(err){
-console.log('Something went wrong connecting with database',err)
+ console.log('Something went wrong connecting with database',err)
 })
 
 module.exports = mongoose.connection;
